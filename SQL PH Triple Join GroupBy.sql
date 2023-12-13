7b3bc9883ce39e7a159d978ac6a69be108b8e3ba
adarshkhub <67844710+adarshkhub@users.noreply.github.com>
2023-12-13 18:09:07 -0500

    Add files via upload
    Triple join practice PH dataset

---
+++ SQL PH Triple Join GroupBy.sql
@@ -0,0 +1,30 @@
+select s.emp_no, s.salary, d.department_name, e.dept_no 
+from salaries as s, 
+departments as d,
+dept_emp as e
+where s.emp_no = e.emp_no 
+and e.dept_no = d.dept_no
+
+-- Find the average salary per department using the groupby function
+
+select * from tempemployeedeptsalary
+
+select dept_name, avg(salary)
+from tempemployeedeptsalary
+Group By dept_name;
+
+
+-- Triple join
+
+Insert into tempemployeedeptsalary(
+SELECT dept_emp.emp_no, salaries.salary, dept_emp.dept_no, departments.dept_name
+FROM dept_emp
+INNER JOIN salaries
+ON dept_emp.emp_no = salaries.emp_no
+INNER JOIN departments
+ON departments.dept_no = dept_emp.dept_no)
+
+Truncate tempemployeedeptsalary
+
+
+
